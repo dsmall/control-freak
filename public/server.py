b94c18d588a190113f14cca815570cefef458641
dsmall <davids@hlh.co.uk>
2012-09-16 14:38:47 +0000

    Update for ACE, support for doc tab, minor fix to sprinkler

--- public/server.py
+++ public/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from uwsgidecorators import *
-import time
+import os, pytronics, time
 
 public = Flask(__name__)
 public.config['PROPAGATE_EXCEPTIONS'] = True
@@ -11,28 +11,68 @@ ALLOWED_DIRECTORIES = set(['static/uploads/', 'static/pictures/'])
 LIVE_PINS = ['LED', '2', '3', '4', '5', '8', '9', '10', '11', '12', '13']
 # public.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
+### Home page ###
+@public.route('/')
+@public.route('/index.html')
+def default_page():
+    with open('/etc/hostname', 'r') as f:
+        name = f.read().strip().capitalize()
+    return render_template('/index.html', hostname=name, template_list = get_public_templates())
+
+def get_public_templates():
+    r = []
+    d = '/var/www/public/templates'
+    for f in os.listdir(d):
+        ff=os.path.join(d,f)
+        if os.path.isfile(ff):
+            r.append(f)
+    return sorted(r)
+
+### Generic HTML and Markdown templates, support for doc tab ###
+@public.route('/<template_name>.html')
+def template(template_name):
+    return render_template(template_name + '.html', magic="Hey presto!")
+
+@public.route('/<doc_name>.markdown')
+def document(doc_name):
+    import markdown2
+    with open('/var/www/public/templates/' + doc_name + '.markdown', 'r') as mdf:
+        return render_template('markdown.html', markdown=markdown2.markdown(mdf.read()))
+    return 'Not Found', 404
+
+@public.route('/get_markdown', methods=['POST'])
+def get_markdown():
+    import markdown2
+    doc_name = request.form['docName']
+    try:
+        with open('/var/www/public/templates/' + doc_name + '.markdown', 'r') as mdf:
+            return markdown2.markdown(mdf.read())
+    except:
+        with open('/var/www/public/templates/default.markdown', 'r') as mdf:
+            return markdown2.markdown(mdf.read())
+    return 'Internal server error', 500
+
+### Support for pins ###
 def toggle_pin(pin):
-    from pytronics import digitalRead, digitalWrite
-    if digitalRead(pin) == '1':
-        digitalWrite(pin, 'LOW')
+    if pytronics.digitalRead(pin) == '1':
+        pytronics.digitalWrite(pin, 'LOW')
     else:
-        digitalWrite(pin, 'HIGH')
+        pytronics.digitalWrite(pin, 'HIGH')
 
 @public.route('/pin/<pin>/<state>')
 def update_pin(pin, state):
-    from pytronics import digitalWrite, pinMode
     try:
         if state.lower() == 'on':
-            digitalWrite(pin, 'HIGH')
+            pytronics.digitalWrite(pin, 'HIGH')
             return 'Set pin %s high' % pin
         elif state.lower() == 'off':                       
-            digitalWrite(pin, 'LOW')
+            pytronics.digitalWrite(pin, 'LOW')
             return 'Set pin %s low' % pin
         elif state.lower() == 'in':
-            pinMode(pin,'INPUT')
+            pytronics.pinMode(pin,'INPUT')
             return 'Set pin %s input' % pin
         elif state.lower() == 'out':
-            pinMode(pin,'OUTPUT')
+            pytronics.pinMode(pin,'OUTPUT')
             return 'Set pin %s output' % pin
         return "Something's wrong with your syntax. You should send something like: /pin/2/on"
     except:
@@ -40,17 +80,16 @@ def update_pin(pin, state):
 
 @public.route('/read-pins', methods=['POST'])
 def read_pins():
-    from pytronics import readPins
     import json
-    return json.dumps(readPins(LIVE_PINS))
-
+    return json.dumps(pytronics.readPins(LIVE_PINS))
+        
+### Support for i2c ###
 @public.route('/i2cget/<addr>/<reg>/<mode>')
 def i2cget(addr, reg, mode):
-    from pytronics import i2cRead
     iaddr = int(addr, 0)
     ireg = int(reg, 0)
     try:
-        res = i2cRead(iaddr, ireg, mode)
+        res = pytronics.i2cRead(iaddr, ireg, mode)
         print '## i2cget ## {0}'.format(res)
         return str(res)
     except (OSError, IOError) as e:
@@ -62,12 +101,11 @@ def i2cget(addr, reg, mode):
 
 @public.route('/i2cset/<addr>/<reg>/<val>/<mode>')
 def i2cset(addr, reg, val, mode):
-    from pytronics import i2cWrite
     iaddr = int(addr, 0)
     ireg = int(reg, 0)
     ival = int(val, 0)
     try:
-        i2cWrite(iaddr, ireg, ival, mode)
+        pytronics.i2cWrite(iaddr, ireg, ival, mode)
         print '## i2cset ##'
         return str(0)
     except (OSError, IOError) as e:
@@ -83,39 +121,52 @@ def i2cscan():
     import json
     return json.dumps(scanBus())
 
-#@rbtimer(60)
-def fetch_calendar(num):
-    import thermostat
-    thermostat.update_calendar_file()
-    print('Calendar reload attempt')
+### Support for SPI bus ###
+@public.route('/spi/<channel>/read')
+def spi_read(channel):
+    if str(channel) not in ['0', '1', '2', '3']:
+        return "You seem to be trying to read from SPI channel {0}, which does not exist. Try 0, 1, 2, or 3.".format(channel)
+    return pytronics.spiRead(channel)
 
-#@rbtimer(3)
-def update_relay(num):
-    import pytronics, thermostat
-    actual = float(thermostat.read_sensor(0x48)) * 1.8 + 32.0
-    target = float(thermostat.get_target_temp('/var/www/public/static/basic.ics', 'America/New_York'))
-    print("Measured temperature: %f degrees. Target is %f degrees." % (actual, target))
-    if actual < target:
-        pytronics.digitalWrite(2, 'HIGH')
-        print("Heat on")
-    else:
-        pytronics.digitalWrite(2, 'LOW')
-        print("Heat off")
+@public.route('/spi/<channel>/write/<data>')
+def spi_write():
+    if str(channel) not in ['0', '1', '2', '3']:
+        return "You seem to be trying to write to SPI channel {0}, which does not exist. Try 0, 1, 2, or 3.".format(channel)
+    return pytronics.spiWrite(data, channel)
 
-@public.route('/<template_name>.html')
-def template(template_name):
-    return render_template(template_name + '.html', magic="Hey presto!")
+@public.route('/spi/<channel>/speed/', defaults={'target': ''})
+@public.route('/spi/<channel>/speed/<target>')
+def spi_speed(channel, target):
+    if str(channel) not in ['0', '1', '2', '3']:
+        return "You seem to be trying to use SPI channel {0}, which does not exist. Try 0, 1, 2, or 3.".format(channel)
+    if str(target) == '':
+        return str(pytronics.spiGetSpeed(str(channel)))
+    return str(pytronics.spiSetSpeed(target, channel))
 
+### Specific demos ###
+# analog-graph
+@public.route('/analog', methods=['POST'])
+def analog():
+    import json
+    try:
+        ad_ref = float(request.form['adref'])
+    except KeyError:
+        ad_ref = 3.3
+    data = {
+        "time" : float(time.time()),
+        "A0" : float(pytronics.analogRead('A0')) * ad_ref / 1024.0
+    }
+    return json.dumps(data)
+
+# relay (also calls analog)
 @public.route('/relay.html')
 def index():
-    import pytronics
     pin = pytronics.digitalRead(2)
-    (chan0, chan1, chan2, chan3) = pytronics.summarize_analog_data()
+    (chan0, chan1, chan2, chan3) = [pytronics.analogRead(chan) for chan in ['A0', 'A1', 'A2', 'A3']]
     return render_template('/relay.html', chan0=chan0, chan1=chan1, chan2=chan2, chan3=chan3, pin=pin)
 
 @public.route('/toggle', methods=['POST'])
 def toggle():
-    import pytronics
     if(request.form['target_state'] == '1'):
         pytronics.digitalWrite(2, 'HIGH')
         result = 'Pins set high'
@@ -126,6 +177,8 @@ def toggle():
         result = 'Target_state is screwed up'
     return result
 
+# thermostat
+#@rbtimer(60)
 @public.route('/temperature', methods=['POST'])
 def temperature():
     import json, thermostat
@@ -136,32 +189,46 @@ def temperature():
     }
     return json.dumps(data)
 
-@public.route('/analog', methods=['POST'])
-def analog():
-    from pytronics import analogRead
-    import json, time
-    try:
-        ad_ref = float(request.form['adref'])
-    except KeyError:
-        ad_ref = 3.3
-    data = {
-        "time" : float(time.time()),
-        "A0" : float(analogRead('A0')) * ad_ref / 1024.0
-    }
-    return json.dumps(data)
+def fetch_calendar(num):
+    import thermostat
+    thermostat.update_calendar_file()
+    print('Calendar reload attempt')
 
+#@rbtimer(3)
+def update_relay(num):
+    import thermostat
+    actual = float(thermostat.read_sensor(0x48)) * 1.8 + 32.0
+    target = float(thermostat.get_target_temp('/var/www/public/static/basic.ics', 'America/New_York'))
+    print("Measured temperature: %f degrees. Target is %f degrees." % (actual, target))
+    if actual < target:
+        pytronics.digitalWrite(2, 'HIGH')
+        print("Heat on")
+    else:
+        pytronics.digitalWrite(2, 'LOW')
+        print("Heat off")
+
+@public.route('/sms', methods=['POST'])
+def parse_sms():
+    import subprocess
+    message = request.form['Body']
+    print "Received text message: " + str(message)
+    f = open('/var/www/public/thermostat-target.txt', 'w')
+    f.write(str(message))
+    f.close()
+    return ('Message processed')
+
+# lcd (serial)
 @public.route('/send-to-lcd', methods=['POST'])
 def send_to_lcd():
-    import pytronics
     pytronics.serialWrite(request.form['serial_text'], 9600)
     return render_template('/lcd.html')
 
 @public.route('/clear-lcd', methods=['POST'])
 def clear_lcd():
-    import pytronics
     pytronics.serialWrite(chr(0xFE) + chr(0x01), 9600)
     return render_template('/lcd.html')
 
+# ck (Color Kinetics) - also called by blinkm
 @public.route('/set-color', methods=['POST'])
 def set_color():
     import colorsys, kinet, subprocess
@@ -187,17 +254,18 @@ def parse_sms():
     f.close()
     return ('Message processed')
 
+# sprinkler
 @public.route('/sprinkler', methods=['POST'])
 def sprinkler():
-    import pytronics
     command = request.form['command']
     if(command == "ON"):
         pytronics.digitalWrite(2, 'HIGH')
     else:
         pytronics.digitalWrite(2, 'LOW')
     return ('Sprinkler toggled')
+### End of specific demo procedures
 
-##### The following procedures support sending email via SMTP #####
+### The following procedures support sending email via SMTP ###
 # They are used by email.html. Configure smtp settings in smtp_lib.py
 @public.route('/')
 @public.route('/email.html')
@@ -223,9 +291,9 @@ def send_email():
         "message" : result[1]
     }
     return json.dumps(data)
-##### End of email procedures
+### End of email procedures
 
-##### The following procedures support file upload #####
+### The following procedures support file upload ###
 # They are called from rascal-1.03.js and used by upload-cf.html, upload-dd.html and album.html
 def allowed_file(filename):
     return '.' in filename and \
@@ -303,26 +371,24 @@ def clear_directory():
     except Exception, e:
         print '## clear_directory ## {}'.format(e)
     return 'Bad request', 400
-##### End of upload procedures #####
+### End of upload procedures ###
 
 # Called from hello.html
 @public.route('/flash_led', methods=['POST'])
 def flash_led():
-    from pytronics import digitalRead, digitalWrite
-    if digitalRead('LED') == '1':
-        digitalWrite('LED', 'LOW')
+    if pytronics.digitalRead('LED') == '1':
+        pytronics.digitalWrite('LED', 'LOW')
         message = "LED off"
     else:
-        digitalWrite('LED', 'HIGH')
+        pytronics.digitalWrite('LED', 'HIGH')
         message = "LED on"
     return (message)
 
 # Called from hello-TMP102.html
 @public.route('/read_temp', methods=['POST'])
 def read_temp():
-    from pytronics import i2cRead
     try:
-        temp = i2cRead(0x48, 0, 'I', 2)
+        temp = pytronics.i2cRead(0x48, 0, 'I', 2)
         strTemp = '{0:0.1f}{1}C'.format(((temp[0] << 4) | (temp[1] >> 4)) / 16.0, unichr(176))
         return strTemp
     except (OSError, IOError) as e:
@@ -335,11 +401,10 @@ def read_temp():
 # dsmall private branch
 @rbtimer(5)
 def toggle_led(num):
-    from pytronics import digitalRead, digitalWrite
-    if digitalRead('LED') == '1':
-        digitalWrite('LED', 'LOW')
+    if pytronics.digitalRead('LED') == '1':
+        pytronics.digitalWrite('LED', 'LOW')
     else:
-        digitalWrite('LED', 'HIGH')
+        pytronics.digitalWrite('LED', 'HIGH')
 
 @cron(0, -6, -1, -1, -1)
 def ntp_daemon(num):
@@ -354,21 +419,20 @@ def ntp_daemon(num):
 
 @public.route('/scope_analog', methods=['POST'])
 def scope_analog():
-    from pytronics import analogRead, digitalRead, i2cRead
-    import json, time
+    import json
     try:
         ad_ref = float(request.form['adref'])
     except KeyError:
         ad_ref = 3.3
-    if digitalRead('LED') == '1':
+    if pytronics.digitalRead('LED') == '1':
         strLED = "LED is on"
     else:
         strLED = "LED is off"
-    temp = i2cRead(0x48, 0, 'I', 2)
+    temp = pytronics.i2cRead(0x48, 0, 'I', 2)
     strTemp = '{0:0.1f}{1}C'.format(((temp[0] << 4) | (temp[1] >> 4)) * 0.0625, unichr(176))
     data = {
         "time" : float(time.time()),
-        "A0" : float(analogRead('A0')) * ad_ref / 1024.0,
+        "A0" : float(pytronics.analogRead('A0')) * ad_ref / 1024.0,
         "date" : time.strftime("%d %b %Y %H:%M:%S %Z", time.localtime()),
         "led" : strLED,
         "temp" : strTemp
@@ -389,13 +453,11 @@ lastUpdate = 12
 
 @rbtimer(5)
 def update_lcd(num):
-    from pytronics import i2cRead
     from i2c_lcd import reset, writeString, setCursor
-    import time
     reset()
     writeString(time.strftime("%H:%M:%S %Z", time.localtime()))
     setCursor(1, 0)
-    temp = i2cRead(0x48, 0, 'I', 2)
+    temp = pytronics.i2cRead(0x48, 0, 'I', 2)
     writeString('Temp {0:0.1f}'.format(((temp[0] << 4) | (temp[1] >> 4)) * 0.0625))
     writeString(chr(0xdf) + 'C')
 
@@ -422,14 +484,13 @@ lastUpdate = 'Not known'
 @public.route('/headlines', methods=['POST'])
 def headlines():
     global headlines, lastFeed, lastSlot, lastUpdate
-    from pytronics import i2cRead
     from news import getHeadlines
-    import json, time
+    import json
     try:
         feed = int(request.form['feed'])
     except KeyError:
         feed = 0
-    temp = i2cRead(0x48, 0, 'I', 2)
+    temp = pytronics.i2cRead(0x48, 0, 'I', 2)
     strTemp = '{0:0.1f}{1}C'.format(((temp[0] << 4) | (temp[1] >> 4)) * 0.0625, unichr(176))
     now = time.localtime()
     # Update headlines every five minutes
@@ -456,8 +517,6 @@ def BCD(b):
 
 def set_rtc ():
     import subprocess
-    import time
-    from pytronics import i2cWrite
     cmd = 'ntpdate uk.pool.ntp.org'
     subp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     try:
@@ -473,25 +532,24 @@ def set_rtc ():
         rtc.append(BCD(t.tm_year - 2000))
         i2cWrite(0x68, 0, rtc, 'I')
         # Reset OSF (oscillator stopped flag)
-        i2cWrite(0x68, 0x09, 0, 'B')
+        pytronics.i2cWrite(0x68, 0x09, 0, 'B')
     except:
         print '## set_rtc ## Failed to set time'
 
 @public.route('/rtc', methods=['POST'])
 def rtc():
-    from pytronics import i2cRead, i2cWrite
     import json
     if 'command' in request.form:
         command = request.form['command']
         if command == 'set_rtc':
             set_rtc()
         elif command == 'start':
-            i2cWrite(0x68, 0, i2cRead(0x68, 0) & 0x7f)
+            pytronics.i2cWrite(0x68, 0, i2cRead(0x68, 0) & 0x7f)
         elif command == 'stop':
-            i2cWrite(0x68, 0, i2cRead(0x68, 0) | 0x80)
+            pytronics.i2cWrite(0x68, 0, i2cRead(0x68, 0) | 0x80)
         elif command == 'reset':
-            i2cWrite(0x68, 0, [0x80, 0, 0], 'I')
-    return json.dumps(i2cRead(0x68, 0, 'I', 7))
+            pytronics.i2cWrite(0x68, 0, [0x80, 0, 0], 'I')
+    return json.dumps(pytronics.i2cRead(0x68, 0, 'I', 7))
 
 PCA_INC_NONE = 0
 PCA_INC_ALL = 0x80      # 0x00-0x1B (28)
@@ -501,17 +559,16 @@ PCA_INC_BRI_GCR = 0xE0  # 0x02-0x13 (18)
 
 @public.route('/dimmer', methods=['POST'])
 def dimmer():
-    from pytronics import i2cRead, i2cWrite
     import json
     try:
         if 'command' in request.form:
             command = request.form['command']
             if command == 'read_status':
-                return json.dumps(i2cRead(0x40, PCA_INC_ALL | 0, 'I', 28))
+                return json.dumps(pytronics.i2cRead(0x40, PCA_INC_ALL | 0, 'I', 28))
             elif command == 'set_brightness':
                 ireg = int(request.form['register'], 0)
                 ival = int(request.form['value'], 0)
-                i2cWrite(0x40, ireg, ival, 'B')
+                pytronics.i2cWrite(0x40, ireg, ival, 'B')
                 return 'OK', 200
     except Exception as e:
         print '## dimmer ##: {0}'.format(e)
